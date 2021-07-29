6e01c6037b86db3412efbbd9e23d53ba4c817859
Muhammad Asdar MLD <muhammadasdar5@gmail.com>
2021-07-29 10:03:15 +0800

    docker react

--- visits/index.js
+++ visits/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const redis = require("redis");
-
+const process = require("process");
 const app = express();
-const clint = redis.createClient();
+const clint = redis.createClient({ host: "redis-server", port: 6379 });
 clint.set("visits", 0);
 
 app.get("/", (req, res) => {
+  process.exit(0);
   clint.get("visits", (err, visits) => {
-    res.send("Number of visitors is" + visits);
+    res.send("Number of visitors is " + visits);
     clint.set("visits", parseInt(visits) + 1);
   });
 });
